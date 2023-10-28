e8be5468c6856c2a37f3a206b794890c0db3a668
y.kurita <kurita.yuya1@gmail.com>
2023-10-28 13:45:15 +0900

    タスク管理/作成画面

--- GoalBuddies/GoalBuddies/settings.py
+++ GoalBuddies/GoalBuddies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-83)8u-f*er)=tsxuh)*-_y)cargfb0dc-m%=s4mdcsvof1sl*+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kajiyai.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "account",
+    "MakeTask",
 ]
 
 MIDDLEWARE = [
