66eafbffd0f5dce1779d859f94fceb0e89245ef0
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-10-28 06:31:57 +0000

    moduleを入れた

--- GoalBuddies/GoalBuddies/wsgi.py
+++ GoalBuddies/GoalBuddies/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for GoalBuddies project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/kajiyai/kajiyai.pythonanywhere.com/GoalBuddies'  # 例: '/home/kajiyai/GoalBuddies'
+if path not in sys.path:
+    sys.path.append(path)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GoalBuddies.settings")
-
-application = get_wsgi_application()
+sys.path.append('/home/kajiyai/kajiyai.pythonanywhere.com/myenv/lib/python3.9/site-packages')
