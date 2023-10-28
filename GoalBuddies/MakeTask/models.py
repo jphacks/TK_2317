ba61a83ca04cd7ddbbbb3c36d9b95e82c64a1325
y.kurita <kurita.yuya1@gmail.com>
2023-10-28 12:08:41 +0900

    タスク追加/編集/削除機能

--- GoalBuddies/MakeTask/models.py
+++ GoalBuddies/MakeTask/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    TAG_CHOICES = [
+        ('programming', 'プログラミング'),
+        ('exam', '受験'),
+        ('assignment', '課題'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    detail = models.TextField()
+    duration = models.IntegerField()  # 日数で管理
+    tags = models.CharField(max_length=50, choices=TAG_CHOICES)  # 選択肢で管理
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- GoalBuddies/MakeTask/views.py
+++ GoalBuddies/MakeTask/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import TaskForm
+from .models import Task
+from django.shortcuts import get_object_or_404, redirect
 
 @login_required
 def account_view(request):
     return render(request, 'MakeTask/account.html')
+
+@login_required
+def mtask_view(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            new_task = form.save(commit=False)
+            new_task.user = request.user  # ユーザーを設定
+            new_task.save()
+            form.save_m2m()  # ManyToManyフィールドを保存
+            return redirect('account')  # accountページへリダイレクト
+    else:
+        form = TaskForm()  # 空のフォームを作成
+
+    return render(request, 'MakeTask/mtask.html', {'form': form})
+
+@login_required
+def account_view(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'MakeTask/account.html', {'tasks': tasks})
+
+def edit_task_view(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'MakeTask/edit_task.html', {'form': form})
+
+@login_required
+def remove_task_view(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    task.delete()
+    return redirect('account')
